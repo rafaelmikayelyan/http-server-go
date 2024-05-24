32e74f7b4c48c1b5d7237df3a38562e2b31b816a
Rafael Mikayelyan <50862318+rafaelmikayelyan@users.noreply.github.com>
2024-05-23 20:58:56 -0700

    feat: add http response

--- app/server.go
+++ app/server.go
@@ -8,16 +8,18 @@ import (
 
 func main() {
 	fmt.Println("Logs from your program will appear here!")
-	
-	 l, err := net.Listen("tcp", "0.0.0.0:4221")
-	 if err != nil {
-	 	fmt.Println("Failed to bind to port 4221")
-	 	os.Exit(1)
-	 }
-	
-	 _, err = l.Accept()
-	 if err != nil {
-	 	fmt.Println("Error accepting connection: ", err.Error())
-	 	os.Exit(1)
-	 }
+
+	l, err := net.Listen("tcp", "0.0.0.0:4221")
+		if err != nil {
+		fmt.Println("Failed to bind to port 4221")
+		os.Exit(1)
+	}
+
+	conn, err := l.Accept()
+		if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
