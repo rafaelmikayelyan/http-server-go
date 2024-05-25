090932257a964dc2b60bded95242d220d2110d4c
Rafael Mikayelyan <50862318+rafaelmikayelyan@users.noreply.github.com>
2024-05-24 18:26:46 -0700

    feat: add URL extraction

--- app/server.go
+++ app/server.go
@@ -1,25 +1,37 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
+	"net/http"
 	"os"
 )
 
 func main() {
-	fmt.Println("Logs from your program will appear here!")
-
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	listener, err := net.Listen("tcp", "0.0.0.0:4221")
 		if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 
-	conn, err := l.Accept()
+	connection, err := listener.Accept()
 		if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
 
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	request, err := http.ReadRequest(bufio.NewReader(connection))
+
+	if err != nil {
+		fmt.Println("Error reading request: ", err.Error())
+		os.Exit(1)
+	}
+
+	if request.URL.Path == "/" {
+		connection.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		return
+	}
+
+	connection.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 }
