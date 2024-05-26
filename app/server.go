73ca7cd978e87508360d6dc7e1a6e1cafc1de4d6
Rafael Mikayelyan <50862318+rafaelmikayelyan@users.noreply.github.com>
2024-05-25 18:32:50 -0700

    feat: add response w/ body

--- app/server.go
+++ app/server.go
@@ -10,26 +10,29 @@ import (
 
 func main() {
 	listener, err := net.Listen("tcp", "0.0.0.0:4221")
-		if err != nil {
+	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 
 	connection, err := listener.Accept()
-		if err != nil {
+	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-
+	
 	request, err := http.ReadRequest(bufio.NewReader(connection))
-
 	if err != nil {
 		fmt.Println("Error reading request: ", err.Error())
 		os.Exit(1)
 	}
-
-	if request.URL.Path == "/" {
+	
+	path := request.URL.Path
+	if path == "/" {
 		connection.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		return
+	} else if path[0:6] == "/echo/" {
+		connection.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(path[6:]), path[6:])))
 		return
 	}
 
